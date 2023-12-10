1e8cfe95f0b8b7d49fa1d8220896ebf990ac3271
4326_Gusti Ayu Putu Febriyanti <gustiayu@students.amikom.ac.id>
2023-12-10 11:01:00 +0700

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { PrismaClient } = require("@prisma/client");
 
 const app = express();
-const port = 4000;
+const PORT = process.env.PORT || 5000
 const prisma = new PrismaClient();
 
 app.use(cors());
@@ -26,6 +26,6 @@ require('./src/routes/writer.routes')(app);
 require('./src/routes/article.routes')(app);
 require('./src/routes/categories.routes')(app);
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log("Server is running....")
 })
